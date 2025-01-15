96db677dcea42f8317889748509404608d062e08
Barney Potter <barney.potter.24@gmail.com>
2025-01-15 11:04:21 +0100

    print version

--- quickbuild.sh
+++ quickbuild.sh
@@ -1,5 +1,2 @@
 make damnthatsreallyclean
-pdflatex -interaction=nonstopmode -synctex=1 -shell-escape thesis
-bibtex thesis
-pdflatex -interaction=nonstopmode -synctex=1 -shell-escape thesis
-pdflatex -interaction=nonstopmode -synctex=1 -shell-escape thesis
+python run.py
